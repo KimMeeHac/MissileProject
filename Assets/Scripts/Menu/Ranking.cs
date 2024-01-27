40c1ec58d2ce580c0dc1ebc6b47af8f0fac258f0
noble-shake <coppgabliw@gmail.com>
2024-01-27 18:44:07 +0900

    127nn

--- Assets/Scripts/Menu/Ranking.cs
+++ Assets/Scripts/Menu/Ranking.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,11 +8,16 @@ public class Ranking : MonoBehaviour
 {
     [SerializeField] Button Delete;
     [SerializeField] Button Return;
-    [SerializeField] Canvas Rankinglist;
+    [SerializeField] Canvas Menu;
+    [SerializeField] List<TextMeshProUGUI> Rankinglist;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Return.onClick.AddListener(() =>
+        {
+            Menu.gameObject.SetActive(true);
+            transform.gameObject.SetActive(false);
+        });
     }
 
     // Update is called once per frame
